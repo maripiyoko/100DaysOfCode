b61bf0cf10a56dddfa4ef8b94701b1623ab128f5
Mariko <maripiyok@gmail.com>
2018-11-01 21:06:12 +0900

    fix style

--- src/components/Day001.vue
+++ src/components/Day001.vue
@@ -16,6 +16,8 @@ export default {
 
 <style scoped lang="sass">
 div
-  border: 1px solid black
-
+  border: none
+  border-radius: 10px
+  background-color: #FFFBFA
+  box-shadow: 2px 2px 3px #f0ddd7
 </style>
