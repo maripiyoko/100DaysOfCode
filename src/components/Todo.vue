cbef399974ae24b8f0b47c7d85e3ac7df8822024
Mariko <maripiyok@gmail.com>
2018-11-05 21:55:51 +0900

    fix style

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   li
-    label
+    label.name
       input(type="checkbox" :checked="status" @input="$emit('input', $event.target.checked)")
       | {{name}}
     a(class="delete" @click="$emit('delete')" title="delete it!")
@@ -13,3 +13,19 @@ export default {
   props: [ 'name', 'status' ]
 }
 </script>
+
+<style lang="sass" scoped>
+li
+  display: flex
+  flex-direction: row
+
+.name
+  flex-grow: 10
+
+.delete
+  color: #FF6667
+  width: 10px
+
+a
+  cursor: pointer
+</style>
