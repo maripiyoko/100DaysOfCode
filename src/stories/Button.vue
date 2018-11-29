6c1da168edbabe4f1a7a4b9b30a509a622904d7c
Mariko <maripiyok@gmail.com>
2018-11-29 23:01:02 +0900

    Add style block to vue

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -1,3 +1,17 @@
 <template lang="pug">
-  button
+  button.button(:class="size")
+    slot
 </template>
+
+<script>
+export default {
+  props: [ 'size' ]
+}
+</script>
+
+<style lang="sass" scoped>
+.button
+  border: 1px solid dimgray
+  border-radius: 15px
+  cursor: pointer
+</style>
