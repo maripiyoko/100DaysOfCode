1af61ef5ec1ee2a43cbcf6f89e167affdd8dc376
Mariko <maripiyok@gmail.com>
2018-11-29 22:59:49 +0900

    Add style loader.
    ref : https://storybook.js.org/configurations/custom-webpack-config/

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,16 @@
+const path = require("path");
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.pug$/,
         loader: 'pug-plain-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, "../")
       }
     ],
   },
